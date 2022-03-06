16af61eebd76b2db241de39264e161421adee85f
karoltylenda <karoltylenda@gmail.com>
2022-03-06 10:16:08 +0100

    add security config with datasource

--- src/main/java/com/ktdev/movie_webapp/HomeController.java
+++ src/main/java/com/ktdev/movie_webapp/HomeController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "Hello World!";
+        return "index";
     }
 
 }
---
+++ src/main/java/com/ktdev/movie_webapp/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ktdev.movie_webapp.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, TRUE from user where username=?")
+                .authoritiesByUsernameQuery("select username, role from user where username=?")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/register", "/css/**").permitAll()
+                .anyRequest().authenticated()
+//                .antMatchers("/index").hasAuthority("ADMIN")
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/index", true).permitAll()
+                .and()
+                .logout().invalidateHttpSession(true).permitAll();
+    }
+}
