3da8f05afb23597bc527afdc2eaf096ae8dd75ac
karoltylenda <karoltylenda@gmail.com>
2022-03-15 23:15:44 +0100

    add profile method to HomeController

--- src/main/java/com/ktdev/movie_webapp/HomeController.java
+++ src/main/java/com/ktdev/movie_webapp/HomeController.java
@@ -1,12 +1,23 @@
 package com.ktdev.movie_webapp;
 
+import com.ktdev.movie_webapp.model.UserDTO;
+import com.ktdev.movie_webapp.model.UserRole;
+import com.ktdev.movie_webapp.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 
 @Controller
 public class HomeController {
+
+    private final UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String main() {
@@ -18,4 +29,11 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/profile")
+    public String profile(Model model, Principal principal){
+        UserDTO user = userService.getByUsername(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("role", UserRole.values());
+        return "profile";
+    }
 }
